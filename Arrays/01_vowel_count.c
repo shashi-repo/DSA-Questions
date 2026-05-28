e680e46ef3d5abec43db86718839f1cbf0db3cbe
Shashi Bhushan <shashibhushan96082@gmail.com>
2026-05-28 11:22:48 +0530

    Added 20 DSA coding questions

---
+++ Arrays/01_vowel_count.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    printf("Enter any string:\n");
+    scanf("%s",str);
+    int n=strlen(str);
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||
+        str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+        count++;
+    }
+    printf("%d",count);
+    return 0;
+}
